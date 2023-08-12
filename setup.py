6261fe9bfc1e41534c6253d39b5c767212b62a3c
Malte J. Ziebarth <mjz.science@fmvkb.de>
2023-08-13 00:54:24 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Setup script.
+#
+# Author: Malte J. Ziebarth (mjz.science@fmvkb.de)
+#
+# Copyright (C) 2023 Malte J. Ziebarth
+#
+# This code is licensed under the MIT license (see LICENSE).
+
+
+from setuptools import setup
+from mebuex import MesonExtension, build_ext
+
+ext = MesonExtension('shgofast.vertex', builddir='builddir')
+
+setup(ext_modules=[ext], cmdclass={'build_ext' : build_ext})
